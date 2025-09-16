7d3a318d8f9b33741343f9a64b6e9c6ad1f1d8d9
snt94 <eduardo775445@gmail.com>
2025-09-15 21:45:14 -0300

    refactor: moved pages from outside the '/pages'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center flex-col min-h-screen min-w-screen">
       <h1 className="text-2xl font-bold">Home</h1>
       <p>Bem-vindo ao painel principal!</p>
     </div>
